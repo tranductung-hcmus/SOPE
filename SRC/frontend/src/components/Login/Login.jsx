3e8c9213e53e363db0047a1d02920fede6fd2e3e
ltkoanh <ltkoanh22@clc.fitus.edu.vn>
2024-08-10 01:10:03 +0700

    Setup Login component with imports, state and form submission.

---
+++ SRC/frontend/src/components/Login/Login.jsx
@@ -0,0 +1,35 @@
+import { React, useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+import styles from "../../styles/styles";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { server } from "../../server";
+import { toast } from "react-toastify";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    await axios
+      .post(
+        `${server}/user/login-user`,
+        {
+          email,
+          password,
+        },
+        { withCredentials: true }
+      )
+      .then((res) => {
+        toast.success("Login Success!");
+        navigate("/");
+        window.location.reload(true); 
+      })
+      .catch((err) => {
+        toast.error(err.response.data.message);
+      });
+  };
