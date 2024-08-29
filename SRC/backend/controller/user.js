d373cc244c9d6050332ae19da21f27c377d4ab41
htthuan <142084558+htthuan2004@users.noreply.github.com>
2024-08-29 16:04:43 +0700

    Update user.js

--- SRC/backend/controller/user.js
+++ SRC/backend/controller/user.js
@@ -112,14 +112,14 @@ router.post(
       const user = await User.findOne({ email }).select("+password");
 
       if (!user) {
-        return next(new ErrorHandler("User doesn't exists!", 400));
+        return next(new ErrorHandler("Please provide the correct information!", 400));
       }
 
       const isPasswordValid = await user.comparePassword(password);
 
       if (!isPasswordValid) {
         return next(
-          new ErrorHandler("Please provide the correct information", 400)
+          new ErrorHandler("Please provide the correct information!", 400)
         );
       }
 
